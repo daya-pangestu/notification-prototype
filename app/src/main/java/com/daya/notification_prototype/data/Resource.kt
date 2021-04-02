bbccfe0e4b17c8ed008ed036591a0165d8700be6
ninggahaken <daya.aeist@gmail.com>
2021-04-02 16:54:58 +0700

    add Resource.kt

--- app/src/main/java/com/daya/notification_prototype/data/Resource.kt
+++ app/src/main/java/com/daya/notification_prototype/data/Resource.kt
@@ -1,4 +1,7 @@
 package com.daya.notification_prototype.data
 
-class Resource {
+sealed class Resource<out T: Any> {
+    data class Success<out T: Any>(val data: T): Resource<T>()
+    data class Error(val exception: Throwable): Resource<Nothing>()
+    data class Loading<out T>(var progress : String?): Resource<Nothing>()
 }
