ab10b4c2d7c271d9cf534ebe6429161c9f23248f
ad <da>
2021-04-13 04:15:41 +0700

    add SplashActivity.kt

---
+++ app/src/main/java/com/daya/notification_prototype/view/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.daya.notification_prototype.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.daya.notification_prototype.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
